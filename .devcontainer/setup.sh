d28e9f00f3f704df1a18aa27565df3721dbd164b
Evan Tahler <evan@evantahler.com>
2025-07-11 22:36:39 -0700

    codespaces

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,6 +2,8 @@
 
 echo "--- CONFIURING CODESPACE ---"
 
+sudo apt-get update -y
+
 # configure bun
 curl -fsSL https://bun.com/install | bash
 bun install
@@ -12,4 +14,6 @@ docker run -p 6379:6379 --name redis -d redis
 
 # configure postgres
 sudo apt-get install postgresql-client -y
-docker run -p 5432:5432 --name postgres -d postgres
+docker run -p 5432:5432 -e POSTGRES_PASSWORD=password --name postgres -d postgres
+PGPASSWORD=password psql -h localhost -U postgres -c "CREATE DATABASE botholomew;"
+PGPASSWORD=password psql -h localhost -U postgres -c "CREATE DATABASE botholomew_test;"
